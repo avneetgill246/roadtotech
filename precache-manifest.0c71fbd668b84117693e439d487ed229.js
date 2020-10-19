0388c9ed7c4fbd619f7328614ca15d4947736e87
Avneet Gill <avneetsinghgill246@gmail.com>
2020-10-20 00:06:40 +0530

    Updates

---
+++ precache-manifest.0c71fbd668b84117693e439d487ed229.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372b16284c95a3cdaa205283c0beb491",
+    "url": "/roadtotech/index.html"
+  },
+  {
+    "revision": "61d2edc122df3d59dbd5",
+    "url": "/roadtotech/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "a7920d7cd128c3f9c73e",
+    "url": "/roadtotech/static/css/main.fd7c2662.chunk.css"
+  },
+  {
+    "revision": "61d2edc122df3d59dbd5",
+    "url": "/roadtotech/static/js/2.23f1e895.chunk.js"
+  },
+  {
+    "revision": "f7def617c168a3e9270f21ef8a75c08f",
+    "url": "/roadtotech/static/js/2.23f1e895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7920d7cd128c3f9c73e",
+    "url": "/roadtotech/static/js/main.5401398b.chunk.js"
+  },
+  {
+    "revision": "9b962fd9d19a966f9d50",
+    "url": "/roadtotech/static/js/runtime-main.4936b878.js"
+  },
+  {
+    "revision": "4ce3c492bd6184c0c07b3b02cccff1dd",
+    "url": "/roadtotech/static/media/coding.4ce3c492.svg"
+  },
+  {
+    "revision": "a74add0b86b8d63427cf5430e2e1220e",
+    "url": "/roadtotech/static/media/d.a74add0b.svg"
+  },
+  {
+    "revision": "1f872be35ced4926addee34d2739e951",
+    "url": "/roadtotech/static/media/f.1f872be3.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://avneetgill246.github.io/roadtotech/precache-manifest.dbb99505833a16fc770d8701607c37ff.js"
+  "/roadtotech/precache-manifest.0c71fbd668b84117693e439d487ed229.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://avneetgill246.github.io/roadtotech/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/roadtotech/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
