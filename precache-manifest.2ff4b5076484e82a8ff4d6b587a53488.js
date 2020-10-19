c8f297c9469677d787fe7aefe4404eed7646b021
Avneet Gill <avneetsinghgill246@gmail.com>
2020-10-19 22:47:59 +0530

    Updates

---
+++ precache-manifest.2ff4b5076484e82a8ff4d6b587a53488.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25940955246e9002c489d5d774874b8c",
+    "url": "/roadtotech/index.html"
+  },
+  {
+    "revision": "fcadd011cc7772974920",
+    "url": "/roadtotech/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "0bd2839fbc9b87d3476e",
+    "url": "/roadtotech/static/css/main.fd7c2662.chunk.css"
+  },
+  {
+    "revision": "fcadd011cc7772974920",
+    "url": "/roadtotech/static/js/2.4caac9f1.chunk.js"
+  },
+  {
+    "revision": "f7def617c168a3e9270f21ef8a75c08f",
+    "url": "/roadtotech/static/js/2.4caac9f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd2839fbc9b87d3476e",
+    "url": "/roadtotech/static/js/main.fcb78f30.chunk.js"
+  },
+  {
+    "revision": "9b962fd9d19a966f9d50",
+    "url": "/roadtotech/static/js/runtime-main.4936b878.js"
+  },
+  {
+    "revision": "4ce3c492bd6184c0c07b3b02cccff1dd",
+    "url": "/roadtotech/static/media/coding.4ce3c492.svg"
+  },
+  {
+    "revision": "a74add0b86b8d63427cf5430e2e1220e",
+    "url": "/roadtotech/static/media/d.a74add0b.svg"
+  },
+  {
+    "revision": "1f872be35ced4926addee34d2739e951",
+    "url": "/roadtotech/static/media/f.1f872be3.svg"
+  }
+]);
